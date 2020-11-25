381b23f64d3c24af7c33a0236b20511475996817
Sallai András <termih@gmail.com>
2020-11-25 20:10:19 +0100

    A super() metódus

---
+++ OOP/Mernok/mernok.py
@@ -0,0 +1,19 @@
+
+
+class Dolgozo:
+    def munka(self):
+        print('ások')
+
+class Mernok(Dolgozo):
+    def munka(self):
+        print('mér')
+    
+    def asas(self):
+        super().munka()
+
+joska = Dolgozo()    
+joska.munka()
+
+mari = Mernok()
+mari.asas()
+    
